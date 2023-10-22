31a7a7c0933d918bc443a98999f4ff4bdd506468
naoexisto bb <machadomarcos2019@gmail.com>
2023-10-22 15:58:56 -0300

    Export Client

--- index.ts
+++ index.ts
@@ -3,10 +3,23 @@ import Refunds from './src/structures/Refunds'
 import Payments from './src/structures/Payments'
 import Cards from './src/structures/Cards'
 import Customers from './src/structures/Customers'
-export default Client
+import PaymentStructureOptions from './src/interfaces/payments/PaymentStrucutureOptions'
+
+export default class PerfectPago {
+    payments: Payments;
+    refunds: Refunds;
+    customers: Customers;
+    constructor(options: PaymentStructureOptions) {
+        this.payments = new Payments({ accessToken: options.accessToken});
+        this.refunds = new Refunds({ accessToken: options.accessToken})
+        this.customers = new Customers({ accessToken: options.accessToken})
+    }
+}
+
 export {
     Refunds,
     Payments,
     Cards,
-    Customers
+    Customers,
+    Client
 }
