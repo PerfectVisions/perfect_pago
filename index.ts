8926911466129ebfcb43d44b7c576ff3f294ab43
naoexisto bb <machadomarcos2019@gmail.com>
2023-10-24 06:00:26 -0300

    Added Oauth to index.ts

--- index.ts
+++ index.ts
@@ -5,14 +5,17 @@ import Cards from './src/structures/Cards'
 import Customers from './src/structures/Customers'
 import PaymentStructureOptions from './src/interfaces/payments/PaymentStrucutureOptions'
 import PaymentMethods from './src/structures/PaymentsMethods'
+import Oauth from './src/structures/Oauth'
 export default class PerfectPago {
     payments: Payments;
     refunds: Refunds;
     customers: Customers;
+    oauth: Oauth;
     constructor(options: PaymentStructureOptions) {
         this.payments = new Payments({ accessToken: options.accessToken});
-        this.refunds = new Refunds({ accessToken: options.accessToken})
-        this.customers = new Customers({ accessToken: options.accessToken})
+        this.refunds = new Refunds({ accessToken: options.accessToken});
+        this.customers = new Customers({ accessToken: options.accessToken});
+        this.oauth = new Oauth({ accessToken: options.accessToken});
     }
 }
 
